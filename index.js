4a3e8c9a6e9e4b87d6117d1e15ae29b624bc269a
Abimael Garcia <abimael1992@gmail.com>
2021-08-11 23:35:14 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import studentRoutes from './routes/student.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "20mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+
+app.use(cors());
+app.use('/students', studentRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://admin:Gama1992!@mycluster.jkhvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => app.listen(PORT, () =>
+    console.log(`connection is stablished and running on ${PORT}`)
+)).catch((err) => console.log(err));
+
+mongoose.set('useFindAndModify', false);
