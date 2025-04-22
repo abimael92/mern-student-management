bd0da2b7332372a241c609cdf5bfd1b2d187af6a
Abimael <abimael1992g@gmail.com>
2025-04-22 03:03:05 -0600

    fix: temporary commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage';
-import DashboardPage from './pages/DashboardPage';
-import StudentForm from './components/StudentForm';
-import StudentList from './components/StudentList'; // Correct import
-
-// Import theme and other Material-UI components
-import { Container, ThemeProvider, CssBaseline, Box } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from './styles/theme';
-
+import MainLayout from './components/layout/MainLayout';
+import appRoutes from './routes/routes';
+import { AuthProvider } from './features/context/AuthContext';
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Router>
-        <Box
-          sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
-        >
-          <Header />
-          <Box component="main" sx={{ flex: 1 }}>
-            <Container maxWidth="lg">
-              <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/dashboard" element={<DashboardPage />} />
-                <Route path="/students" element={<StudentList />} />
-                <Route path="/add-student" element={<StudentForm />} />
-                <Route path="/edit-student/:id" element={<StudentForm />} />
-              </Routes>
-            </Container>
-          </Box>
-          <Footer />
-        </Box>
-      </Router>
-    </ThemeProvider>
+    <AuthProvider>
+      {' '}
+      {/* Wrap the app with AuthProvider */}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <MainLayout>
+            <Routes>
+              {appRoutes.map(({ path, element }, idx) => (
+                <Route key={idx} path={path} element={element} />
+              ))}
+            </Routes>
+          </MainLayout>
+        </Router>
+      </ThemeProvider>
+    </AuthProvider>
   );
 };
 
