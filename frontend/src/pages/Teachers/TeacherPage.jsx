17a9b42b4534e7df16149b2bea64903e7a2dc997
Abimael <abimael1992g@gmail.com>
2025-05-06 01:50:48 -0600

    fix teachers page

---
+++ frontend/src/pages/Teachers/TeacherPage.jsx
@@ -0,0 +1,12 @@
+// pages/Students/TeacherPage.jsx
+import React from 'react';
+import { Box } from '@mui/material';
+import TeacherContainer from '../../components/teachers/TeacherContainer';
+
+const TeacherPage = () => (
+  <Box sx={{ mt: 4 }}>
+    <TeacherContainer />
+  </Box>
+);
+
+export default TeacherPage;
