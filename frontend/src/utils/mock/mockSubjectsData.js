dd79418da015db492527a1bb370f748ae2efa413
Abimael <abimael1992g@gmail.com>
2025-05-13 02:00:04 -0600

    add mock data

---
+++ frontend/src/utils/mock/mockSubjectsData.js
@@ -0,0 +1,46 @@
+export const mockSubjects = [
+    {
+        name: 'Mathematics',
+        gradeTrend: [
+            { term: 'Term 1', grade: 75 },
+            { term: 'Term 2', grade: 80 },
+            { term: 'Term 3', grade: 85 },
+            { term: 'Term 4', grade: 90 },
+        ],
+        passed: 120,
+        failed: 30,
+    },
+    {
+        name: 'Science',
+        gradeTrend: [
+            { term: 'Term 1', grade: 65 },
+            { term: 'Term 2', grade: 70 },
+            { term: 'Term 3', grade: 72 },
+            { term: 'Term 4', grade: 78 },
+        ],
+        passed: 110,
+        failed: 40,
+    },
+    {
+        name: 'English',
+        gradeTrend: [
+            { term: 'Term 1', grade: 85 },
+            { term: 'Term 2', grade: 88 },
+            { term: 'Term 3', grade: 90 },
+            { term: 'Term 4', grade: 92 },
+        ],
+        passed: 130,
+        failed: 20,
+    },
+    {
+        name: 'History',
+        gradeTrend: [
+            { term: 'Term 1', grade: 70 },
+            { term: 'Term 2', grade: 75 },
+            { term: 'Term 3', grade: 78 },
+            { term: 'Term 4', grade: 80 },
+        ],
+        passed: 100,
+        failed: 50,
+    },
+];
