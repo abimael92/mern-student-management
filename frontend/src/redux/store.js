f96f88646417c81b084f2c37ca5f0333398a92c7
Abimael <abimael1992g@gmail.com>
2025-05-12 23:19:14 -0600

    add subjects to store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,10 +2,14 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { thunk } from 'redux-thunk';
 
 import { studentReducer } from './reducers/studentReducer';
+import { subjectReducer } from './reducers/subjectsReducers';
+
 import { teacherReducer } from './reducers/teacherReducer';
 
 const rootReducer = combineReducers({
     students: studentReducer,
+    subjects: subjectReducer,
+
     teachers: teacherReducer,
 });
 
