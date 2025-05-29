cc17090bf76ed18edd336ec4b5e3ea083262c771
Abimael <abimael1992g@gmail.com>
2025-05-28 18:50:25 -0600

    add form  of courses to academics page

--- frontend/src/pages/Academics/AcademicsPage.jsx
+++ frontend/src/pages/Academics/AcademicsPage.jsx
@@ -4,6 +4,8 @@ import SubjectManagement from '../../components/academic/SubjectManagement';
 import SubjectsPerformanceOverview from '../../components/academic/SubjectsPerformanceOverview';
 import SubjectsStatusView from '../../components/academic/SubjectsStatusView';
 import CourseList from '../../components/academic/CourseList';
+import CourseForm from '../../components/academic/CourseForm'; // inside
+
 import GradeHistory from '../../components/academic/GradeHistory';
 
 import { courses, gradeHistory } from '../../utils/mock/AcademicsPage';
@@ -14,25 +16,30 @@ const AcademicsPage = () => {
       <Typography variant="h4" gutterBottom>
         Academics Overview
       </Typography>
-
       <Paper sx={{ p: 2, mt: 2 }}>
         <Typography variant="h6">Courses</Typography>
         <CourseList courses={courses} />
       </Paper>
-
+      AcademicsPage return, add above CourseList
+      <Paper sx={{ p: 2, mt: 2 }}>
+        <Typography variant="h6">Add New Course</Typography>
+        <CourseForm
+          onSubmit={(newCourse) => {
+            // handle new course submit here (e.g. API call, state update)
+            console.log('New course data:', newCourse);
+          }}
+        />
+      </Paper>
       <Paper sx={{ p: 2, mt: 2 }}>
         <SubjectManagement />
       </Paper>
-
       {/* Subjects & Performance Overview */}
       <Paper sx={{ p: 2, mt: 2 }}>
         <SubjectsPerformanceOverview />
       </Paper>
-
       <Paper sx={{ p: 2, mt: 2 }}>
         <SubjectsStatusView />
       </Paper>
-
       <Paper sx={{ p: 2, mt: 2 }}>
         <Typography variant="h6">Grades</Typography>
         <GradeHistory history={gradeHistory} />
