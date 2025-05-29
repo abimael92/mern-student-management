c8c9528a77a84e0ad33301b193e2642f74a38a12
Abimael <abimael1992g@gmail.com>
2025-05-28 18:50:37 -0600

    imrove the course list

--- frontend/src/components/academic/CourseList.jsx
+++ frontend/src/components/academic/CourseList.jsx
@@ -7,28 +7,40 @@ import {
   TableHead,
   TableRow,
   Paper,
+  IconButton,
 } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const CourseList = ({ courses }) => (
+const CourseList = ({ courses, onEdit, onDelete }) => (
   <TableContainer component={Paper}>
     <Table size="small">
       <TableHead>
         <TableRow>
-          <TableCell>Course Code</TableCell>
-          <TableCell>Course Name</TableCell>
+          <TableCell>Code</TableCell>
+          <TableCell>Name</TableCell>
           <TableCell>Instructor</TableCell>
           <TableCell>Semester</TableCell>
           <TableCell>Grade</TableCell>
+          <TableCell>Actions</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
-        {courses.map((course, index) => (
-          <TableRow key={index}>
+        {courses.map((course) => (
+          <TableRow key={course.id || course._id || course.code}>
             <TableCell>{course.code}</TableCell>
             <TableCell>{course.name}</TableCell>
-            <TableCell>{course.instructor}</TableCell>
+            <TableCell>{course.instructor?.name || '-'}</TableCell>
             <TableCell>{course.semester}</TableCell>
             <TableCell>{course.grade}</TableCell>
+            <TableCell>
+              <IconButton onClick={() => onEdit(course)}>
+                <EditIcon />
+              </IconButton>
+              <IconButton onClick={() => onDelete(course._id)}>
+                <DeleteIcon />
+              </IconButton>
+            </TableCell>
           </TableRow>
         ))}
       </TableBody>
