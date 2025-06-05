17f30037b14d2693526fa79c29ce6a145a0b4de4
Abimael <abimael1992g@gmail.com>
2025-06-04 20:46:18 -0600

    fix filter

--- frontend/src/components/academic/GPAStudentFilter.jsx
+++ frontend/src/components/academic/GPAStudentFilter.jsx
@@ -1,41 +1,25 @@
+// GPAStudentFilter.jsx
 import React from 'react';
 import { Box, Grid, TextField, MenuItem } from '@mui/material';
 
-const StudentFilter = ({
-  courseFilter,
-  setCourseFilter,
+const GPAStudentFilter = ({
   subjectFilter,
   setSubjectFilter,
+  courseFilter,
+  setCourseFilter,
   studentNameFilter,
   setStudentNameFilter,
-  dateRangeFilter,
-  setDateRangeFilter,
-  courses = [],
-  subjects = [],
+  subjects,
+  courses,
 }) => {
+  const filteredCourses = courses.filter(
+    (course) => subjectFilter && course.subjectId === subjectFilter
+  );
+
   return (
     <Box sx={{ p: 2 }}>
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={3}>
-          <TextField
-            select
-            label="Course"
-            value={courseFilter}
-            onChange={(e) => setCourseFilter(e.target.value)}
-            fullWidth
-          >
-            <MenuItem value="">All</MenuItem>
-            {(courses || []).map((course) => (
-              <MenuItem
-                key={course.id ?? course.name}
-                value={course.id != null ? course.id.toString() : ''}
-              >
-                {course.name}
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
+        <Grid item xs={12} sm={4}>
           <TextField
             select
             label="Subject"
@@ -44,17 +28,32 @@ const StudentFilter = ({
             fullWidth
           >
             <MenuItem value="">All</MenuItem>
-            {(subjects || []).map((subject) => (
-              <MenuItem
-                key={subject.id ?? subject.name}
-                value={subject.id != null ? subject.id.toString() : ''}
-              >
-                {subject.name}
+            {subjects.map((s) => (
+              <MenuItem key={`subject-${s.id}`} value={s.id}>
+                {s.name}
               </MenuItem>
             ))}
           </TextField>
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
+        {subjectFilter && (
+          <Grid item xs={12} sm={4}>
+            <TextField
+              select
+              label="Course"
+              value={courseFilter}
+              onChange={(e) => setCourseFilter(e.target.value)}
+              fullWidth
+            >
+              <MenuItem value="">All</MenuItem>
+              {filteredCourses.map((c) => (
+                <MenuItem key={`course-${c.id}`} value={c.id}>
+                  {c.name}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Grid>
+        )}
+        <Grid item xs={12} sm={4}>
           <TextField
             label="Student Name"
             value={studentNameFilter}
@@ -62,19 +61,9 @@ const StudentFilter = ({
             fullWidth
           />
         </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <TextField
-            label="Date"
-            type="date"
-            value={dateRangeFilter}
-            onChange={(e) => setDateRangeFilter(e.target.value)}
-            fullWidth
-            InputLabelProps={{ shrink: true }}
-          />
-        </Grid>
       </Grid>
     </Box>
   );
 };
 
-export default StudentFilter;
+export default GPAStudentFilter;
