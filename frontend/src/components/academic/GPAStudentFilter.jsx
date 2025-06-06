8f759df371be75d42dccd9590b000df18b7a6aca
Abimael <abimael1992g@gmail.com>
2025-06-05 19:03:52 -0600

    fix filter selection

--- frontend/src/components/academic/GPAStudentFilter.jsx
+++ frontend/src/components/academic/GPAStudentFilter.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Box, Grid, TextField, MenuItem } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { fetchSubjects } from '../../redux/actions/subjectsActions';
+import { fetchCourses } from '../../redux/actions/coursesActions';
+import { fetchTeachers } from '../../redux/actions/teacherActions';
 
-const GPAStudentFilter = ({
-  subjectFilter,
-  setSubjectFilter,
-  courseFilter,
-  setCourseFilter,
-  teacherFilter,
-  setTeacherFilter,
-  studentNameFilter,
-  setStudentNameFilter,
-  semesterFilter,
-  setSemesterFilter,
-  startDate,
-  setStartDate,
-  endDate,
-  setEndDate,
-  subjects,
-  courses,
-  teachers,
-}) => {
+const GPAStudentFilter = ({ filters, onFilterChange }) => {
+  const dispatch = useDispatch();
+
+  // Get data directly from Redux store with proper initial state
+  const subjects = useSelector((state) => state.subjects?.subjects || []);
+  const courses = useSelector((state) => state.courses?.courses || []);
+  const teachers = useSelector((state) => state.teachers?.teachers || []);
+
+  // Fetch data on component mount
+  useEffect(() => {
+    dispatch(fetchSubjects());
+    dispatch(fetchCourses());
+    dispatch(fetchTeachers());
+  }, [dispatch]);
+
+  const {
+    subjectFilter = '',
+    courseFilter = '',
+    teacherFilter = '',
+    studentNameFilter = '',
+    semesterFilter = '',
+    startDate,
+    endDate,
+  } = filters;
+
+  const updateFilter = (key, value) => {
+    onFilterChange({
+      ...filters,
+      [key]: value === undefined ? '' : value,
+    });
+  };
+
   return (
     <Box sx={{ p: 2 }}>
       <Grid container spacing={2}>
@@ -30,13 +47,13 @@ const GPAStudentFilter = ({
             select
             label="Subject"
             value={subjectFilter}
-            onChange={(e) => setSubjectFilter(e.target.value)}
+            onChange={(e) => updateFilter('subjectFilter', e.target.value)}
             fullWidth
           >
-            <MenuItem value="">All</MenuItem>
-            {subjects.map((s) => (
-              <MenuItem key={s.id} value={s.id}>
-                {s.name}
+            <MenuItem value="">All Subjects</MenuItem>+{' '}
+            {subjects.map((subject) => (
+              <MenuItem key={subject._id} value={subject._id}>
+                {subject.name}
               </MenuItem>
             ))}
           </TextField>
@@ -48,13 +65,13 @@ const GPAStudentFilter = ({
             select
             label="Course"
             value={courseFilter}
-            onChange={(e) => setCourseFilter(e.target.value)}
+            onChange={(e) => updateFilter('courseFilter', e.target.value)}
             fullWidth
           >
-            <MenuItem value="">All</MenuItem>
-            {courses.map((c) => (
-              <MenuItem key={c.id} value={c.id}>
-                {c.name}
+            <MenuItem value="">All Courses</MenuItem>+{' '}
+            {courses.map((course) => (
+              <MenuItem key={course._id} value={course._id}>
+                {course.name}
               </MenuItem>
             ))}
           </TextField>
@@ -66,54 +83,51 @@ const GPAStudentFilter = ({
             select
             label="Teacher"
             value={teacherFilter}
-            onChange={(e) => setTeacherFilter(e.target.value)}
+            onChange={(e) => updateFilter('teacherFilter', e.target.value)}
             fullWidth
           >
-            <MenuItem value="">All</MenuItem>
-            {teachers.map((t) => (
-              <MenuItem key={t.id} value={t.id}>
-                {t.name}
+            <MenuItem value="">All Teachers</MenuItem>+{' '}
+            {teachers.map((teacher) => (
+              <MenuItem key={teacher._id} value={teacher._id}>
+                {`${teacher.firstName} ${teacher.lastName}`}
               </MenuItem>
             ))}
           </TextField>
         </Grid>
 
-        {/* Student Name Filter */}
+        {/* Other filters remain the same */}
         <Grid item xs={12} sm={3}>
           <TextField
             label="Student Name"
             value={studentNameFilter}
-            onChange={(e) => setStudentNameFilter(e.target.value)}
+            onChange={(e) => updateFilter('studentNameFilter', e.target.value)}
             fullWidth
           />
         </Grid>
 
-        {/* Semester Filter */}
         <Grid item xs={12} sm={3}>
           <TextField
             label="Semester / Grade"
             value={semesterFilter}
-            onChange={(e) => setSemesterFilter(e.target.value)}
+            onChange={(e) => updateFilter('semesterFilter', e.target.value)}
             fullWidth
           />
         </Grid>
 
-        {/* Start Date Picker */}
         <Grid item xs={12} sm={3}>
           <DatePicker
             label="Start Date"
             value={startDate}
-            onChange={(date) => setStartDate(date)}
+            onChange={(newDate) => updateFilter('startDate', newDate)}
             slotProps={{ textField: { fullWidth: true } }}
           />
         </Grid>
 
-        {/* End Date Picker */}
         <Grid item xs={12} sm={3}>
           <DatePicker
             label="End Date"
             value={endDate}
-            onChange={(date) => setEndDate(date)}
+            onChange={(newDate) => updateFilter('endDate', newDate)}
             slotProps={{ textField: { fullWidth: true } }}
           />
         </Grid>
