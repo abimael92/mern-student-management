16dc494c39fae0fa2b8c5ec13a88b58186afb5d9
Abimael <abimael1992g@gmail.com>
2025-06-11 16:48:24 -0600

    fix display of subject management

--- frontend/src/components/academic/SubjectManagement.jsx
+++ frontend/src/components/academic/SubjectManagement.jsx
@@ -7,32 +7,25 @@ import {
   deleteSubject,
 } from '../../redux/actions/subjectsActions';
 
-import { fetchTeachers } from '../../redux/actions/teacherActions';
-import { fetchStudents } from '../../redux/actions/studentActions';
-
 import SubjectList from './SubjectList';
 import SubjectForm from './SubjectForm';
 import LoadingSpinner from '../common/LoadingSpinner';
 import ErrorAlert from '../common/ErrorAlert';
 
-import { Typography, Paper, Grid, Box } from '@mui/material';
+import { Typography, Grid, Box } from '@mui/material';
 
 const SubjectManagement = () => {
   const dispatch = useDispatch();
-  const subjectsState = useSelector((state) => state.subjects || {});
-  const teachersState = useSelector((state) => state.teachers || {});
-  const studentsState = useSelector((state) => state.students || {});
-
-  const { subjects = [], loading = false, error = null } = subjectsState;
-  const { teachers = [] } = teachersState;
-  const { students = [] } = studentsState;
+  const {
+    subjects = [],
+    loading = false,
+    error = null,
+  } = useSelector((state) => state.subjects || {});
 
   const [selectedSubject, setSelectedSubject] = useState(null);
 
   useEffect(() => {
     dispatch(fetchSubjects());
-    dispatch(fetchTeachers());
-    dispatch(fetchStudents());
   }, [dispatch]);
 
   const handleEditClick = (subject) => setSelectedSubject(subject);
@@ -54,7 +47,7 @@ const SubjectManagement = () => {
   };
 
   return (
-    <div>
+    <Box>
       <Typography variant="h4" gutterBottom>
         Subject Management
       </Typography>
@@ -82,13 +75,11 @@ const SubjectManagement = () => {
               setSelectedSubject={setSelectedSubject}
               onSave={handleSave}
               onCancel={() => setSelectedSubject(null)}
-              teachers={teachers}
-              students={students}
             />
           </Box>
         </Grid>
       </Grid>
-    </div>
+    </Box>
   );
 };
 
