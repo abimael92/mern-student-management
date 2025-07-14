853a3d12f187594fb39787147d2e2887a4eb6438
Abimael <abimael1992g@gmail.com>
2025-07-14 17:30:00 -0600

    refactor(redux): update academic state management

--- frontend/src/redux/actions/index.js
+++ frontend/src/redux/actions/index.js
@@ -1,8 +1,11 @@
 // src/redux/actions/index.js
 
-// Export all action creators from each file
+export * from './classesActions';
 export * from './coursesActions';
 export * from './gradesActions';
 export * from './studentActions';
 export * from './subjectsActions';
 export * from './teacherActions';
+export * from './roomsActions';
+export * from './semesterActions';
+
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,21 +4,19 @@ import { thunk } from 'redux-thunk';
 import { studentReducer } from './reducers/studentReducer';
 import { subjectReducer } from './reducers/subjectsReducers';
 import { coursesReducer } from './reducers/coursesReducer';
-
-
+import { classesReducer } from "./reducers/classesReducer";
+import { roomsReducer } from "./reducers/roomsReducer";
 import { teacherReducer } from './reducers/teacherReducer';
-
 import { gradesReducer } from './reducers/gradesReducer';
 
 const rootReducer = combineReducers({
     subjects: subjectReducer,
     courses: coursesReducer,
+    classes: classesReducer,
     grades: gradesReducer,
-
+    rooms: roomsReducer,
     teachers: teacherReducer,
     students: studentReducer,
-
-
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
