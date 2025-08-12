979e9d2b952190303edc47cee8cd4f3094a81763
Abimael Garcia <abimael1992g@gmail.com>
2025-08-12 17:06:24 -0600

    delete: remove deprecated room routes

--- backend/routes/room.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createRoom, getRooms } from '../controllers/rooms/room.controller.js';
-
-const router = express.Router();
-
-router.post('/', createRoom);
-router.get('/', getRooms);
-
-export default router;
