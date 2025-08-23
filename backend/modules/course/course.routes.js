c9c703d2557f9e85dfda28227b4604e9bf49cf47
Abimael Garcia <abimael1992g@gmail.com>
2025-08-22 20:58:58 -0600

    add: new course routes

---
+++ backend/modules/course/course.routes.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+    getCourses,
+    getCourseWithSubjects,
+    createCourse,
+    updateCourse,
+    assignSubjectToCourse,
+    deleteCourse,
+} from './course.controller.js';
+
+const router = express.Router();
+
+// ----- READ -----
+router.get("/", getCourses);
+router.get("/:id/with-subjects", getCourseWithSubjects);
+
+// ----- CREATE -----
+router.post("/", createCourse);
+
+// ----- UPDATE -----
+router.put("/:id", updateCourse);
+router.put("/:id/assign-subject", assignSubjectToCourse); // New endpoint
+
+// ----- DELETE -----
+router.delete("/:id", deleteCourse);
+
+
+
+export default router;
